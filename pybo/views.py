d955465ecbcb63f2161783334a1f77130d31ef03
DESKTOP-EAISUPF\black <blacksw1102@gmail.com>
2021-11-09 00:45:16 +0900

    'URL과 네임스페이스'까지 학습

---
+++ pybo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
+# Create your views here.
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
